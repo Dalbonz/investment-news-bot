273a7a4f76aa6d7997d5c966b98cfa5ad2537a36
Dalbonz <swpark1204@gmail.com>
2026-04-22 16:52:25 +0900

    news_bot.py 업데이트

--- news_bot.py
+++ news_bot.py
@@ -1,6 +1,7 @@
 import os
 import smtplib
 import requests
+import xml.etree.ElementTree as ET
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime
@@ -10,11 +11,11 @@ TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 GMAIL_USER = os.environ['GMAIL_USER']
 GMAIL_PASSWORD = os.environ['GMAIL_PASSWORD']
 
+
 def get_news():
     url = "https://news.google.com/rss/search?q=주식+투자+증시&hl=ko&gl=KR&ceid=KR:ko"
     response = requests.get(url)
     news_items = []
-    import xml.etree.ElementTree as ET
     root = ET.fromstring(response.content)
     for item in root.findall('.//item')[:10]:
         title = item.find('title').text
@@ -22,18 +23,15 @@ def get_news():
         news_items.append({'title': title, 'link': link})
     return news_items
 
-def get_market_data():
-    return (
-        "📊 주요 증시 현황\n"
-        "🇰🇷 코스피: finance.naver.com 확인\n"
-        "🇺🇸 나스닥: finance.yahoo.com 확인\n"
-        "💱 환율: finance.naver.com/marketindex 확인\n"
-    )
 
 def send_telegram(news_items):
     today = datetime.now().strftime("%Y년 %m월 %d일")
     msg = "📈 <b>" + today + " 투자 뉴스 브리핑</b>\n\n"
-    msg += get_market_data() + "\n"
+    msg += "📊 주요 증시 현황\n"
+    msg += "🇰🇷 <a href='https://finance.naver.com/sise'>코스피/코스닥</a>\n"
+    msg += "🇺🇸 <a href='https://finance.yahoo.com'>나스닥/S&P500</a>\n"
+    msg += "💱 <a href='https://finance.naver.com/marketindex'>환율</a>\n"
+    msg += "🛢 <a href='https://www.investing.com/commodities/crude-oil'>유가</a>\n\n"
     msg += "📰 <b>오늘의 주요 뉴스</b>\n"
     for i, item in enumerate(news_items, 1):
         msg += str(i) + ". <a href='" + item['link'] + "'>" + item['title'] + "</a>\n"
@@ -41,8 +39,4 @@ def send_telegram(news_items):
     msg += "• <a href='https://finance.naver.com'>네이버 금융</a>\n"
     msg += "• <a href='https://www.investing.com'>인베스팅닷컴</a>\n"
     msg += "• <a href='https://finance.yahoo.com'>야후 파이낸스</a>"
-    url = "https://api.telegram.org/bot" + TELEGRAM_TOKEN + "/sendMessage"
-    data = {"chat_id": TELEGRAM_CHAT_ID, "text": msg, "parse_mode": "HTML"}
-    requests.post(url, data=data)
-
-def send_email(news_items):
+    url = "https://api.telegram.org/bot" + TELEGRAM
